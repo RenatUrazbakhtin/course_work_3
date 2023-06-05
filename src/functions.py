ff36f014ea89f3165ac781e1b237610313b3f37e
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-06-05 14:15:26 +0300

    creating new function mask_from and testfor

--- src/functions.py
+++ src/functions.py
@@ -25,3 +25,27 @@ def check_executed(data:list[dict]):
         else:
             continue
     return executed_list
+
+def mask_from(input_from_account: str):
+    """
+    форматирует строку в формат: первые 6 цифр читаемые, последние 4 цифры читаемые, остальные символы "*", разделены по пробелам
+    :param input_from_account:
+    :return: форматированную строку
+    """
+    from_account_split = input_from_account.split(" ")
+    if len(from_account_split) == 2:
+        if len(from_account_split[1]) == 16:
+            str = " " + from_account_split[-1][0:4] + " " + from_account_split[-1][4:6] + "** " + "**** " + from_account_split[-1][12:]
+        elif len(from_account_split[1]) == 20:
+            str = " " + from_account_split[-1][0:4] + " " + from_account_split[-1][4:6] + "** " + "**** " + "**** " + from_account_split[-1][16:]
+        else:
+            return "Cannot identify account from"
+        return from_account_split[0] + str
+    if len(from_account_split) == 3:
+        if len(from_account_split[2]) == 16:
+            str = ' ' + from_account_split[-1][0:4] + ' ' + from_account_split[-1][4:6] + '** ' + '**** ' + from_account_split[-1][12:]
+        elif len(from_account_split[2]) == 20:
+            str = " " + from_account_split[1][0:4] + " " + from_account_split[1][4:6] + "** " + "**** " + "**** " + from_account_split[1][16:]
+        else:
+            return "Cannot identify account from"
+        return from_account_split[0] + " " + from_account_split[1] + str
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,5 +1,5 @@
 import pytest
-from src.functions import read_json, check_executed
+from src.functions import read_json, check_executed, mask_from
 def test_read_json(data):
   """
   тест для функции чтения файла из json
@@ -14,3 +14,13 @@ def test_check_executed(data, executed_expected):
   :return:
   """
   assert check_executed(data) == executed_expected
+
+def test_mask_from():
+  """
+  тест для проверки функции mask_from
+  :return:
+  """
+  assert mask_from("Счет 12189246980267075758") == "Счет 1218 92** **** **** 5758"
+  assert mask_from("МИР 4878656375033856") == "МИР 4878 65** **** 3856"
+  assert mask_from("Visa Platinum 8990850370884895") == "Visa Platinum 8990 85** **** 4895"
+  assert mask_from("Visa Gold 3654412434951162") == "Visa Gold 3654 41** **** 1162"
