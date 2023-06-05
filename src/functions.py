7cedb2b7c2591f857d84c78835b6ae1457c91ba6
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-06-05 14:12:17 +0300

    creating new function check_executed and testfor

--- src/functions.py
+++ src/functions.py
@@ -10,3 +10,18 @@ def read_json(filename):
     with open(filename, "r", encoding="utf-8") as file:
         data = json.load(file)
     return data
+
+def check_executed(data:list[dict]):
+    """
+    Проверка словарей по статусу выполнения executed
+    :param data:
+    :return: список словарей со статусом executed
+    """
+    executed_list = []
+    for item in data:
+        if "state" in item.keys():
+            if item["state"] == "EXECUTED":
+                executed_list.append(item)
+        else:
+            continue
+    return executed_list
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,8 +1,16 @@
 import pytest
-from src.functions import read_json
+from src.functions import read_json, check_executed
 def test_read_json(data):
   """
   тест для функции чтения файла из json
   :return:
   """
   assert read_json("test_operations.json") == data
+
+def test_check_executed(data, executed_expected):
+  """
+  тест для проверки статуса операции
+  :param data:
+  :return:
+  """
+  assert check_executed(data) == executed_expected
